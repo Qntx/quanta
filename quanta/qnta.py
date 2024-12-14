4a8b6e26f81cc8822bba77cda19699383af240d4
CtrlX <1438802682@qq.com>
2024-12-14 23:28:56 +0800

    dev

--- quanta/qnta.py
+++ quanta/qnta.py
@@ -2,6 +2,7 @@ import typer
 
 app = typer.Typer()
 
+
 @app.command()
 def hello(name: str):
     """
@@ -9,12 +10,14 @@ def hello(name: str):
     """
     typer.echo(f"Hello {name}!")
 
+
 @app.command()
 def add(a: int, b: int):
     """
     Add two numbers.
     """
     typer.echo(f"The result is {a + b}")
+
 
 if __name__ == "__main__":
     app()
